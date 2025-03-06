e8bc2b421fd657f829baec1d8f6df67e4b2b4528
mwaqas0340 <162116714+mwaqas0340@users.noreply.github.com>
2025-03-06 20:05:26 +0500

    Add files via upload

---
+++ opeartor.py
@@ -0,0 +1,165 @@
+
+# a : int = 8
+# b : int = 2
+
+# Python Arithmetic Operators
+
+# print(a + b) # addtition
+# print(a - b) # subtraction
+# print(a * b) # multiplication
+# print(a / b) # division
+
+# a: int = 7
+# print(b := 7)
+
+
+
+# Python Assignment Operators
+
+
+a : int = 7
+print(a)
+a = a + 2
+print(a)
+
+
+
+a : int = 7
+print(a)
+a += 2
+print(a)
+
+
+
+a : int = 7
+print(a)
+a -= 2
+print(a)
+
+
+
+
+
+
+
+
+# Python Comparison Operators
+
+# a : int = 7
+# b : int = 10
+# print( a == b )
+
+# a : int = 7
+# b : str = '7'
+# print(a == b)
+
+# a : int = 7
+# b : int = 7
+# print( a == b )
+
+# a : int = 7
+# b : int = 7
+# print( a != b )
+
+
+# a : int = 7
+# b : int = 8
+# print( a < b )
+
+
+
+
+
+
+
+
+# Python Logical Opetator
+
+# and operator
+
+# print( 2 < 3 and 4>1 )
+
+# exp = 2 > 3 and 4 < 5
+# print(exp)
+
+# name = 'waqas'=='rajput' and 'waqas'=='rajput'
+# print(name)
+
+# # or operator
+
+# num = 2 > 5 or 4 < 8
+# print(num)
+
+# a = 5 == 5 or 3 == 4
+# print(a)
+
+
+# # not operator
+
+# name = 'waqas'
+# print(not(name))
+
+
+
+
+
+
+# b = 5 > 3
+# print(not(b))
+
+# name = 'waqas'
+# if name == 'waqa':
+#     print(not(name))
+# else:
+#     print('invalid name')
+    
+    
+# a = 3 == 3 and 2 == 3
+# print(a)
+
+# a = 3 == 3 or 2 == 3
+# print(a)
+
+# a = 3 == 3
+# print(not(a))
+
+
+# password = 12
+# if password == 123:
+#     print("countinue", password)
+#     con = password
+#     print('Login')
+#     name = 'Waqas'
+#     print(name)
+    
+# else:
+#     print("invalid password", password) 
+#     print('try again')
+
+
+# def func(a:str,b:str):
+#  return a + b
+# print('Hello Function')
+# print(func('waqas ','fahham'))
+
+
+
+# a = 5
+# a += 2
+# a -= 3
+# a *= 2
+# a /= 2
+# print('The curent value of "a" is',a)
+
+
+# a = '7'
+# b = 7
+# print(a==b)
+
+
+
+
+
+
+
+
