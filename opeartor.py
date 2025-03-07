d5867e9d2828b86393e4dc25f9ed9b2258fb06ad
mwaqas0340 <162116714+mwaqas0340@users.noreply.github.com>
2025-03-07 14:30:46 +0500

    Update opeartor.py
    a few update in my repo

--- opeartor.py
+++ opeartor.py
@@ -1,10 +1,10 @@
 
 # Python Arithmetic Operators
 
-# print(a + b) # addtition
-# print(a - b) # subtraction
-# print(a * b) # multiplication
-# print(a / b) # division
+print(a + b) # addtition
+print(a - b) # subtraction
+print(a * b) # multiplication
+print(a / b) # division
 
 
 # Python Assignment Operators
@@ -31,68 +31,114 @@ print(a)
 
 # Python Comparison Operators
 
-# a : int = 7
-# b : int = 10
-# print( a == b )
+a : int = 7
+b : int = 10
+print( a == b )
 
-# a : int = 7
-# b : str = '7'
-# print(a == b)
+a : int = 7
+b : str = '7'
+print(a == b)
 
-# a : int = 7
-# b : int = 7
-# print( a == b )
+a : int = 7
+b : int = 7
+print( a == b )
 
-# a : int = 7
-# b : int = 7
-# print( a != b )
+a : int = 7
+b : int = 7
+print( a != b )
 
-# a : int = 7
-# b : int = 8
-# print( a < b )
+a : int = 7
+b : int = 8
+print( a < b )
 
 
 
 # Python Logical Opetator
 
-# and operator
+and operator
 
-# print( 2 < 3 and 4>1 )
+print( 2 < 3 and 4>1 )
 
-# exp = 2 > 3 and 4 < 5
-# print(exp)
-
-
-# # or operator
-
-# num = 2 > 5 or 4 < 8
-# print(num)
-
-# a = 5 == 5 or 3 == 4
-# print(a)
+exp = 2 > 3 and 4 < 5
+print(exp)
 
 
-# # not operator
+# or operator
 
-# name = 'waqas'
-# print(not(name))
+num = 2 > 5 or 4 < 8
+print(num)
+
+a = 5 == 5 or 3 == 4
+print(a)
 
 
-# b = 5 > 3
-# print(not(b))
+# not operator
 
-# name = 'waqas'
-# if name == 'waqa':
-#     print(not(name))
-# else:
-#     print('invalid name')
+name = 'waqas'
+print(not(name))
+
+
+b = 5 > 3
+print(not(b))
+
+name = 'waqas'
+if name == 'waqa':
+    print(not(name))
+else:
+    print('invalid name')
     
     
-# a = 3 == 3 and 2 == 3
-# print(a)
+a = 3 == 3 and 2 == 3
+print(a)
 
-# a = 3 == 3 or 2 == 3
-# print(a)
+a = 3 == 3 or 2 == 3
+print(a)
 
-# a = 3 == 3
-# print(not(a))
+a = 3 == 3
+print(not(a))
+
+
+# Python Identity Opetators 
+
+# is operator
+a = 2
+b = 5
+print(a is b) # False
+
+
+a = 2
+b = 5
+a = b
+print(a is b) # True
+
+
+# is not operator
+a = 2
+b = 5
+print(a is not b) # True
+
+
+a = 2
+b = 5
+a = b
+print(a is not b) # False
+
+
+# Python Membership Opetators
+
+# in operator
+text = 'This is a dummy text'
+print('dummy' in text) # True
+
+
+text = 'This is a dummy text'
+print('python' in text) # False
+
+
+# not in operator
+text = 'This is a dummy text'
+print('dummy' not in text) # False
+
+
+text = 'This is a dummy text'
+print('python' not in text) # True
